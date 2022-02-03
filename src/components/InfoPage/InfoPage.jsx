32887d1f1081fc1c5756632afafbd6b74df9aa48
Jennifer Alexander <jenny_alexander@icloud.com>
2022-02-02 18:04:07 -0600

    Add test component to POST question answers to DB

--- src/components/InfoPage/InfoPage.jsx
+++ src/components/InfoPage/InfoPage.jsx
@@ -1,14 +1,97 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import './InfoPage.css';
 
-// This is one of our simplest components
-// It doesn't have local state
-// It doesn't dispatch any redux actions or display any part of redux state
-// or even care what the redux state is
 
 function InfoPage() {
+  const [answer, setAnswer] = useState('');
+  const [currentQuestion, setCurrentQuestion] = useState();
+  const [policyID, setPolicyID] = useState();
+  const user = useSelector(store => store.user);
+  //const policy = useSelect(store=>store.policy); <-----NEED TO ADD THIS
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    //(1) setCurrentQuestion(); <--For "REAL" component, we should set the policy here when the page renders
+  }, []);
+
+  const onSubmit = () => {
+    //dummy data for now
+    dispatch({
+      type: 'SAVE_TO_BUILDER',
+      payload: {
+        id: policyID,   //<-------POLICY ID should be retrieved from the store (see above)
+        userID: user.id,
+        answers: [{
+          question: currentQuestion,
+          answer: answer
+        }]
+      }
+    })
+  }
+
+  const handleAnswerChange = (event) => {
+    setAnswer(parseInt(event.target.value));
+    setCurrentQuestion(event.target.name); //<---Temporarily setting the question for testing purposes. Should be done in useEffect().
+  }
+  const handlePolicyIDChange = (event) => {
+    setPolicyID(event.target.value);
+  }
+
   return (
-    <div className="container">
-      <p>Info Page</p>
+    <div>
+      {/* <div className="container">
+        <p>Info Page</p>
+      </div> */}
+      <div className="question">
+        <input type="text" name="policy_id" placeholder='Enter a policy #'
+          onChange={(event) => handlePolicyIDChange(event)}>
+        </input>
+        <h3>Reason for travel policy document</h3>
+        <div>
+          <input type="radio"
+            id="answer_1"
+            name="question_1"
+            value="1"
+            onChange={handleAnswerChange}
+          />
+          <label for="answer_1">Placeholder 1</label>
+        </div>
+        <div>
+          <input type="radio"
+            id="answer_1"
+            name="question_1"
+            value="2"
+            onChange={handleAnswerChange} />
+          <label for="answer_1">Placeholder 2</label>
+        </div>
+        <div>
+          <input type="radio"
+            id="answer_1"
+            name="question_1"
+            value="3"
+            onChange={handleAnswerChange} />
+          <label for="answer_1">Placeholder 3</label>
+        </div>
+        <div>
+          <input type="radio"
+            id="answer_1"
+            name="question_1"
+            value="4"
+            onChange={handleAnswerChange} />
+          <label for="answer_1">Placeholder 4</label>
+        </div>
+        <div>
+          <input type="radio"
+            id="answer_1"
+            name="question_1"
+            value="5"
+            onChange={handleAnswerChange} />
+          <label for="answer_1">Placeholder 5</label>
+        </div>
+        <button onClick={onSubmit}>Save</button>
+      </div>
     </div>
   );
 }
--- src/redux/sagas/builder.saga.js
+++ src/redux/sagas/builder.saga.js
@@ -2,25 +2,31 @@ import axios from 'axios';
 import { put, takeLatest } from 'redux-saga/effects';
 import store from '../store';
 
-// 
+
 function* fetchBuilder(action) {
   console.log('Builder saga test')
   // get all builder from the DB
   try {
-      const missing = yield axios.get(`/api/builder`);
-      console.log('get track:', builder.data);
-      yield put({ type: 'SET_BUILDER', payload: builder.data });
+    const missing = yield axios.get(`/api/builder`);
+    console.log('get track:', builder.data);
+    yield put({ type: 'SET_BUILDER', payload: builder.data });
 
   } catch (err) {
-      console.log('get builder error', err);
+    console.log('get builder error', err);
   }
-      
+
 }
 
+function* saveToBuilder(action) {
+  console.log(`in saveToBuilder!`);
+  console.log(`payload is:`, action.payload);
+  const response = yield axios.post(`/api/policy-builder/`, action.payload);
 
+}
 
-function* missingSaga() {
+function* builderSaga() {
   yield takeLatest('FETCH_BUILDER', fetchBuilder)
+  yield takeLatest(`SAVE_TO_BUILDER`, saveToBuilder);
 }
 
 export default builderSaga;
