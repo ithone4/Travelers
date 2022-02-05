f79a3130555576873ab54e05ca4553f0f447b103
Giancarlo Sierra <giancarlosierra2011@hotmail.com>
2022-02-05 11:55:07 -0600

    tooltip and footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,46 @@
 import React from 'react';
 import './Footer.css';
+import LocalHospitalRoundedIcon from '@mui/icons-material/LocalHospitalRounded';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
+import SailingIcon from '@mui/icons-material/Sailing';
+import BusinessIcon from '@mui/icons-material/Business';
+import ParkIcon from '@mui/icons-material/Park';
+import Tooltip from '@mui/material/Tooltip';
 
-// This is one of our simplest components
-// It doesn't have local state, so it can be a function component.
-// It doesn't dispatch any redux actions or display any part of redux state
-// or even care what the redux state is, so it doesn't need 'connect()'
+
+const healthPop = (event) =>{
+ console.log("healthPop")
+}
+
+
+const moneyPop = (event) =>{
+ console.log("moneyPop")
+}
+
+
+const sustainPop= (event) =>{
+ console.log("sustainPop")
+}
+
+
+const experiencePop= (event) =>{
+ console.log("experiencePop")
+}
+
+
+const businessPop =(event)=>{
+ console.log("businessPop")
+}
+
 
 function Footer() {
-  return <footer>&copy; Prime Digital Academy</footer>;
+ return <footer className="footerRow">
+ <Tooltip title="Select this if you company prioritize's health on this question"><LocalHospitalRoundedIcon onClick={healthPop} className="footerIcon"/></Tooltip>
+ <Tooltip title="Select this if you company prioritize's cost on this question"><AttachMoneyIcon onClick={moneyPop} className="footerIcon"/></Tooltip>
+ <Tooltip title="Select this if you company prioritize's sustainability on this question"><ParkIcon onClick={sustainPop} className="footerIcon"/></Tooltip>
+ <Tooltip title="Select this if you company prioritize's experience on this question"><SailingIcon onClick={experiencePop} className="footerIcon"/></Tooltip>
+ <Tooltip title="Select this if you company prioritize's the business process on this question"><BusinessIcon onClick={businessPop} className="footerIcon"/></Tooltip>
+ </footer>;
 }
 
 export default Footer;
