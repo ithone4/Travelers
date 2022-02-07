3d0a3764cc47bbce5f99b1805dabaac974a31fc0
Giancarlo Sierra <giancarlosierra2011@hotmail.com>
2022-02-06 22:00:25 -0600

    answers and radio buttons working on submits

--- src/components/QuestionPage/QuestionAnswers.jsx
+++ src/components/QuestionPage/QuestionAnswers.jsx
@@ -2,17 +2,18 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
-function QuestionItem(props) {
+function QuestionAnswers(props) {
  const [answer, setAnswer] = useState('');
   const [currentQuestion, setCurrentQuestion] = useState();
   // const [policyID, setPolicyID] = useState();
   const answers = useSelector(store => store.answerReducer);
+  const user = useSelector(store => store.user);
   // const [currentQuestion, setCurrentQuestion] = useState(0);
   // //const policy = useSelect(store=>store.policy); <-----NEED TO ADD THIS
   // const dispatch = useDispatch();
 
   useEffect(() => {
-    setAnswer(getAnswersForQuestion(props.question.id));
+    setAnswer(getAnswersForQuestion(props.answer.id));
     console.log(`answers are:`, answers)
   });
 
@@ -49,12 +50,60 @@ function QuestionItem(props) {
 
   return (
     <div>
-      <h4>{props.question.question_text}</h4>
+      <h4>{props.answer.id}</h4>
+      <div>
+      <input type="radio"
+            id="answer_1"
+            name="question_1"
+            value="1"
+            onChange={handleAnswerChange}
+          />
+          <label for="answer_1">{props.answer.answer_1}</label>
+          </div>
 
+          <div>
+      <input type="radio"
+            id="answer_2"
+            name="question_2"
+            value="2"
+            onChange={handleAnswerChange}
+          />
+          <label for="answer_2">{props.answer.answer_2}</label>
+          </div>
+
+          <div>
+      <input type="radio"
+            id="answer_3"
+            name="question_3"
+            value="3"
+            onChange={handleAnswerChange}
+          />
+          <label for="answer_3">{props.answer.answer_3}</label>
+          </div>
+
+          <div>
+      <input type="radio"
+            id="answer_4"
+            name="question_4"
+            value="4"
+            onChange={handleAnswerChange}
+          />
+          <label for="answer_4">{props.answer.answer_4}</label>
+          </div>
+
+          <div>
+      <input type="radio"
+            id="answer_5"
+            name="question_5"
+            value="5"
+            onChange={handleAnswerChange}
+          />
+          <label for="answer_1">{props.answer.answer_5}</label>
+          </div>
 
 
     </div>
   )
 }
 
-export default QuestionItem;
+export default QuestionAnswers;
--- src/components/QuestionPage/QuestionPage.jsx
+++ src/components/QuestionPage/QuestionPage.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import './QuestionPage.css';
 import QuestionItem from './QuestionItem';
+import QuestionAnswers from './QuestionAnswers';
 
 
 function QuestionPage() {
@@ -10,24 +11,21 @@ function QuestionPage() {
   const [currentQuestion, setCurrentQuestion] = useState();
   const [policyID, setPolicyID] = useState();
   const user = useSelector(store => store.user);
-  const questionR = useSelector(store=>store.questionReducer);
-  // const answerList= useSelector(store=>store.answerReducer);
-  // const policyID= useSelector(store=>store.policyBuilderReducer);
+  const questions = useSelector(store => store.questionReducer);
+  let [currentQuestionID, setCurrentQuestionID] = useState(0);
+  const [totalNumOfQuestions, setTotalNumOfQuestions] = useState();
+  const [lastQuestion, setLastQuestion] = useState(false);
+  const answers = useSelector(store => store.answerReducer);
+  //const policy = useSelect(store=>store.policy); <-----NEED TO ADD THIS
   const dispatch = useDispatch();
 
+
   useEffect(() => {
-    dispatch({ type: 'FETCH_BUILDER', payload: user.id}); //dispatch call for policy builder move in future to where it makes sense
+    dispatch({ type: 'FETCH_BUILDER', payload: user.id }); //dispatch call for policy builder move in future to where it makes sense
     //(1) setCurrentQuestion(); <--For "REAL" component, we should set the policy here when the page renders
+    console.log(`in useEffect!`)
+    setTotalNumOfQuestions(questions.length);
   }, []);
-
-  // let thisQuestion= 0
-
-  const add = () =>{
-    let thisQuestion=0;
-    thisQuestion++;
-    return thisQuestion
-    
-  }
 
   const onSubmit = () => {
     //dummy data for now
@@ -37,15 +35,12 @@ function QuestionPage() {
         id: policyID,   //<-------POLICY ID should be retrieved from the store (see above)
         userID: user.id,
         answers: [{
-          question: thisQuestion,
+          question: currentQuestion,
           answer: answer
         }]
       }
     })
-  }
-
-  const onBack = () => {
-    console.log('Hey! This button will take you to the previous page!')
+    goToNextQuestion()
   }
 
   const handleAnswerChange = (event) => {
@@ -55,67 +50,43 @@ function QuestionPage() {
   const handlePolicyIDChange = (event) => {
     setPolicyID(event.target.value);
   }
+  const goToNextQuestion = (event) => {
+    setCurrentQuestionID(currentQuestionID + 1);
+  }
+
+  const goToPreviousQuestion = (event) => {
+    setCurrentQuestionID(currentQuestionID - 1);
+  }
+  const startPolicyBuilder = (event) => {
+    setCurrentQuestionID(0);
+  }
 
   return (
     <div>
-      {/* <div className="container">
-        <p>Info Page</p>
-      </div>
-      <button onClick={add}>Add</button> */}
+      {JSON.stringify(questions[0])}
       <div className="question">
         <input type="text" name="policy_id" placeholder='Enter policy #'
           onChange={(event) => handlePolicyIDChange(event)}>
         </input>
-
-        {/* { questionR.map(( questionR )=>( <QuestionItem questionR={questionR}/>) )} */}
-        <h3>Reason for travel policy document</h3>
-        <div>
-          <input type="radio"
-            id="answer_1"
-            name="question_1"
-            value="1"
-            onChange={handleAnswerChange}
-          />
-          <label for="answer_1">Placeholder 1</label>
-        </div>
-        <div>
-          <input type="radio"
-            id="answer_1"
-            name="question_1"
-            value="2"
-            onChange={handleAnswerChange} />
-          <label for="answer_1">Placeholder 2</label>
-        </div>
-        <div>
-          <input type="radio"
-            id="answer_1"
-            name="question_1"
-            value="3"
-            onChange={handleAnswerChange} />
-          <label for="answer_1">Placeholder 3</label>
-        </div>
-        <div>
-          <input type="radio"
-            id="answer_1"
-            name="question_1"
-            value="4"
-            onChange={handleAnswerChange} />
-          <label for="answer_1">Placeholder 4</label>
-        </div>
-        <div>
-          <input type="radio"
-            id="answer_1"
-            name="question_1"
-            value="5"
-            onChange={handleAnswerChange} />
-          <label for="answer_1">Placeholder 5</label>
-        </div>
-        <div>
-        <button className='questionButton' onClick={onBack}>Back</button>
-        <button className='questionButton' onClick={onSubmit}>Save</button>
-        </div>
+        <p>
+          <button onClick={startPolicyBuilder}>Start Policy Builder</button>
+          <p>Total Number of Questions is: {totalNumOfQuestions}</p>
+        </p>
+        <QuestionItem question={questions[currentQuestionID]}
+        />
+        <QuestionAnswers answer={answers[currentQuestionID]}
+        />
+        <button onClick={goToPreviousQuestion}>
+          Back
+        </button>
+        <button onClick={goToNextQuestion}>
+          Next
+        </button>
+        <p>
+          <button>Submit</button>
+        </p>
       </div>
-    </div>
+    </div >
   );
 }
 
