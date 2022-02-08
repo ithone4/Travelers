808da67bec613b8ff9b8c96f78420b2b128eba55
Giancarlo Sierra <giancarlosierra2011@hotmail.com>
2022-02-08 13:00:41 -0600

    working tooltip

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -49,11 +49,37 @@ function Footer(props) {
 
  return <footer className="footerRow">
      {JSON.stringify(props.question.id)}
- <Tooltip title="Select this if you company prioritize's health on this question"><LocalHospitalRoundedIcon onClick={healthPop} className="footerIcon"/></Tooltip>
- <Tooltip title="Select this if you company prioritize's cost on this question"><AttachMoneyIcon onClick={moneyPop} className="footerIcon"/></Tooltip>
- <Tooltip title="Select this if you company prioritize's sustainability on this question"><ParkIcon onClick={sustainPop} className="footerIcon"/></Tooltip>
- <Tooltip title="Select this if you company prioritize's experience on this question"><SailingIcon onClick={experiencePop} className="footerIcon"/></Tooltip>
- <Tooltip title="Select this if you company prioritize's the business process on this question"><BusinessIcon onClick={businessPop} className="footerIcon"/></Tooltip>
+     {JSON.stringify(props.question.safety)}
+     <div>{props.question.safety===true?
+<Tooltip title="This question is heavily related to health.">
+    <LocalHospitalRoundedIcon className="footerIcon"/>
+    </Tooltip>:
+            <LocalHospitalRoundedIcon disabled className="footerDisabled"/>}
+            </div>
+            <div>{props.question.cost===true?
+<Tooltip title="This question is heavily related to cost">
+    <AttachMoneyIcon className="footerIcon"/>
+    </Tooltip>:
+            <AttachMoneyIcon disabled className="footerDisabled"/>}
+            </div>
+ <div>{props.question.sustainability===true?
+<Tooltip title="This question is heavily related to sustainability.">
+    <ParkIcon className="footerIcon"/>
+    </Tooltip>:
+            <ParkIcon disabled className="footerDisabled"/>}
+            </div>
+            <div>{props.question.experience===true?
+<Tooltip title="This question is heavily related to experience.">
+    <SailingIcon className="footerIcon"/>
+    </Tooltip>:
+            <SailingIcon disabled className="footerDisabled"/>}
+            </div>
+            <div>{props.question.business_processes===true?
+<Tooltip title="This question is heavily related to the business process.">
+    <BusinessIcon className="footerIcon"/>
+    </Tooltip>:
+            <BusinessIcon disabled className="footerDisabled"/>}
+            </div>
  </footer>;
 }
 
