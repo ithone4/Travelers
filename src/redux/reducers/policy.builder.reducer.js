6106137b4b9e095761a989c12b8e5e305072d187
Jennifer Alexander <jenny_alexander@icloud.com>
2022-02-10 22:40:05 -0600

    Setup framework to save policy builder to temp before save

--- src/redux/reducers/policy.builder.reducer.js
+++ src/redux/reducers/policy.builder.reducer.js
@@ -7,25 +7,27 @@ const policyBuilderReducer = (state = [], action) => {
     default:
       return state;
   }
-  return state;
 }
-
+const tempPolicyReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'SET_TEMP_BUILDER':
+      return action.payload;
+    default:
+      return state;
+  }
+}
 const companyCultureReducer = (state = '', action) => {
   switch (action.type) {
     case 'SET_COMPANY_CULTURE':
       console.log(`in companyCultureReducer and the action.payload is:`, action.payload);
       return action.payload;
-    case 'UNSET_COMPANY_CULTURE':
-      return 0;
     default:
       return state;
   }
 };
-// user will be on the redux state at:
-// state.user
-//export default policyBuilderReducer;
 
 export default combineReducers({
   policyBuilderReducer,
   companyCultureReducer,
+  tempPolicyReducer
 });
--- src/redux/sagas/policy.builder.saga.js
+++ src/redux/sagas/policy.builder.saga.js
@@ -2,10 +2,7 @@ import axios from 'axios';
 import { put, takeLatest } from 'redux-saga/effects';
 import store from '../store';
 
-
-
 function* fetchBuilder(action) {
-
   console.log('Builder saga test:', action.payload)
   // get all builder from the DB
   try {
@@ -16,24 +13,26 @@ function* fetchBuilder(action) {
     console.log('get builder error', err);
   }
 }
-
 function* saveToBuilder(action) {
   console.log(`in saveToBuilder!`);
   console.log(`payload is:`, action.payload);
   const response = yield axios.post(`/api/policy-builder/`, action.payload);
 }
-
 function* fetchCompanyCulture(action) {
   console.log(`in fetchCompanyCulture with action.payload`, action.payload);
   const response = yield axios.get(`api/policy-builder/culture/${action.payload}`);
   console.log(`in saga back from GET of company culture. response is:`, response.data[0]);
   yield put({ type: 'SET_COMPANY_CULTURE', payload: response.data[0].culture })
 }
-
+function* saveBuilderToLocal(action) {
+  console.log(`in saveBuilderToLocal with action.payload`, action.payload);
+  yield put({ type: 'SET_TEMP_BUILDER', payload: action.payload })
+}
 function* policyBuilderSaga() {
-  yield takeLatest(`FETCH_BUILDER`, fetchBuilder)
-  yield takeLatest(`SAVE_TO_BUILDER`, saveToBuilder);
+  yield takeLatest(`FETCH_BUILDER`, fetchBuilder) //Get from DB
+  yield takeLatest(`SAVE_TO_BUILDER`, saveToBuilder); //Save to DB
   yield takeLatest('FETCH_COMPANY_CULTURE', fetchCompanyCulture);
+  yield takeLatest('SAVE_BUILDER_TO_LOCAL', saveBuilderToLocal); //Local save
 }
 
 export default policyBuilderSaga;
