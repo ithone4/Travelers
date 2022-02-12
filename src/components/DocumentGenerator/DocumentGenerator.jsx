d5df2354b7a31598666d882e11b733d03bad15b4
petestapp <63821853+petestapp@users.noreply.github.com>
2022-02-12 14:19:48 -0600

    cleaned up basic set up for document generation

--- src/components/DocumentGenerator/DocumentGenerator.jsx
+++ src/components/DocumentGenerator/DocumentGenerator.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import * as docx from "docx";
 import { saveAs } from "file-saver";
@@ -8,7 +8,12 @@ import { create } from '@mui/material/styles/createTransitions';
 
 function DocumentGenerator(props) {
   const store = useSelector((store) => store);
-  const [heading, setHeading] = useState('Functional Component');
+  const [heading, setHeading] = useState('Document Generator');
+
+  useEffect(() => {
+    console.log(`in useEffect`);
+    createDocumentArray();
+  }, []);
 
   let testData = [
     {
@@ -19,12 +24,16 @@ function DocumentGenerator(props) {
       header: "Second Header",
       paragraphs: ["Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, veniam eos? Hic ea esse consequuntur aspernatur sint repudiandae quam fugiat dolores repellendus labore autem eius libero suscipit eveniet, nam tenetur. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur tenetur omnis placeat eveniet animi optio vitae, quae mollitia fuga quos excepturi saepe aliquam, dolorem vel? Dolore blanditiis magni aliquid hic. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate, ex. Velit, eum sed. Asperiores velit quis accusantium temporibus, est molestiae ipsam earum. Hic, eaque quaerat ab veritatis ipsa est architecto.", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, veniam eos? Hic ea esse consequuntur aspernatur sint repudiandae quam fugiat dolores repellendus labore autem eius libero suscipit eveniet, nam tenetur. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur tenetur omnis placeat eveniet animi optio vitae, quae mollitia fuga quos excepturi saepe aliquam, dolorem vel? Dolore blanditiis magni aliquid hic.",]
     },
+    {
+      header: "Third Header",
+      paragraphs: ["Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, veniam eos? Hic ea esse consequuntur aspernatur sint repudiandae quam fugiat dolores repellendus labore autem eius libero suscipit eveniet, nam tenetur. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur tenetur omnis placeat eveniet animi optio vitae, quae mollitia fuga quos excepturi saepe aliquam, dolorem vel? Dolore blanditiis magni aliquid hic. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate, ex. Velit, eum sed. Asperiores velit quis accusantium temporibus, est molestiae ipsam earum. Hic, eaque quaerat ab veritatis ipsa est architecto.", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, veniam eos? Hic ea esse consequuntur aspernatur sint repudiandae quam fugiat dolores repellendus labore autem eius libero suscipit eveniet, nam tenetur. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur tenetur omnis placeat eveniet animi optio vitae, quae mollitia fuga quos excepturi saepe aliquam, dolorem vel? Dolore blanditiis magni aliquid hic.",]
+    },
   ]
 
   const [documentArray, setDocumentArray] = useState([]);
   // 1 = header; 2 = paragraph;
 
-  const proceduralTest = () => {
+  const createDocumentArray = () => {
     for (let i = 0; i < testData.length; i++) {
       documentArray.push(1);
       documentArray.push(testData[i].header);
@@ -33,53 +42,21 @@ function DocumentGenerator(props) {
         documentArray.push(testData[i].paragraphs[j]);
       }
     }
-    console.log("documentArray:", documentArray);
-    // codeGenerator();
-    // console.log(codeGeneratorArray);
   }
 
-  // const [codeGeneratorArray, setcodeGeneratorArray] = useState([]);
-
-  // const codeGenerator = () => {
-  //   for (let i = 0; i < documentArray.length; i++) {
-  //     if (documentArray[i] === 1) {
-  //       let a = `new docx.Paragraph({
-  //         text: ` + documentArray[i + 1] + `,
-  //         heading: HeadingLevel.HEADING_2`;
-  //       // console.log("header:", documentArray[i + 1]);
-  //       codeGeneratorArray.push(a);
-  //     }
-  //     else if (documentArray[i] === 2) {
-  //       let b = `new docx.Paragraph({
-  //           text:` + documentArray[i + 1] + `,
-  //           style: "normalPara",
-  //         }),`
-  //       // console.log("paragraph:", documentArray[i + 1]);
-  //       codeGeneratorArray.push(b);
-  //     }
-  //     else {
-  //       console.log("blah");
-  //     }
-  //   }
-  // }
-
-  const createHeadersAndParagraphs = (array) => {
+  const createChildrenArray = (array) => {
     console.log('in createHeadersAndParagraphs');
-    console.log(documentArray);
     for (let i = 0; i < documentArray.length; i++) {
       if (documentArray[i] === 1) {
-        console.log("header: ", documentArray[i + 1]);
         childrenArray.push(createHeader(documentArray[i + 1]));
       }
       else if (documentArray[i] === 2) {
-        console.log("paragraph:", documentArray[i + 1]);
         childrenArray.push(createParagraph(documentArray[i + 1]));
       }
       else {
-        console.log('tehe')
+        console.log('not important')
       }
     }
-    console.log('childrenArray:', childrenArray);
   }
 
   const createHeader = (element) => {
@@ -98,7 +75,7 @@ function DocumentGenerator(props) {
 
   const [childrenArray, setChildrenArray] = useState([
     new docx.Paragraph({
-      text: "HUH",
+      text: "Travel Policy",
       heading: HeadingLevel.HEADING_1,
     }),
   ]);
@@ -146,35 +123,6 @@ function DocumentGenerator(props) {
           properties: {},
           children:
             childrenArray
-          // [
-          // new docx.Paragraph({
-          //   text: "Travel Policy",
-          //   heading: HeadingLevel.HEADING_1,
-          // }),
-
-          // new docx.Paragraph({
-          //   text: "First Header",
-          //   heading: HeadingLevel.HEADING_2,
-          // }),
-          // new docx.Paragraph({
-          //   text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, veniam eos? Hic ea esse consequuntur aspernatur sint repudiandae quam fugiat dolores repellendus labore autem eius libero suscipit eveniet, nam tenetur. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur tenetur omnis placeat eveniet animi optio vitae, quae mollitia fuga quos excepturi saepe aliquam, dolorem vel? Dolore blanditiis magni aliquid hic. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate, ex. Velit, eum sed. Asperiores velit quis accusantium temporibus, est molestiae ipsam earum. Hic, eaque quaerat ab veritatis ipsa est architecto.",
-          //   style: "normalPara",
-          // }),
-          // new docx.Paragraph({
-          //   text: "Second Header",
-          //   heading: HeadingLevel.HEADING_2,
-          // }),
-          // new docx.Paragraph({
-          //   text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, veniam eos? Hic ea esse consequuntur aspernatur sint repudiandae quam fugiat dolores repellendus labore autem eius libero suscipit eveniet, nam tenetur. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur tenetur omnis placeat eveniet animi optio vitae, quae mollitia fuga quos excepturi saepe aliquam, dolorem vel? Dolore blanditiis magni aliquid hic. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate, ex. Velit, eum sed. Asperiores velit quis accusantium temporibus, est molestiae ipsam earum. Hic, eaque quaerat ab veritatis ipsa est architecto.",
-          //   style: "normalPara",
-          // }),
-          // new docx.Paragraph({
-          //   text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, veniam eos? Hic ea esse consequuntur aspernatur sint repudiandae quam fugiat dolores repellendus labore autem eius libero suscipit eveniet, nam tenetur. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur tenetur omnis placeat eveniet animi optio vitae, quae mollitia fuga quos excepturi saepe aliquam, dolorem vel? Dolore blanditiis magni aliquid hic. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate, ex. Velit, eum sed. Asperiores velit quis accusantium temporibus, est molestiae ipsam earum. Hic, eaque quaerat ab veritatis ipsa est architecto.",
-          //   style: "normalPara",
-          // }),
-          // createHeader('Third Header'),
-          // createParagraph('Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, veniam eos? Hic ea esse consequuntur aspernatur sint repudiandae quam fugiat dolores repellendus labore autem eius libero suscipit eveniet, nam tenetur. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur tenetur omnis placeat eveniet animi optio vitae, quae mollitia fuga quos excepturi saepe aliquam, dolorem vel? Dolore blanditiis magni aliquid hic. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate, ex. Velit, eum sed. Asperiores velit quis accusantium temporibus, est molestiae ipsam earum. Hic, eaque quaerat ab veritatis ipsa est architecto Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, veniam eos? Hic ea esse consequuntur aspernatur sint repudiandae quam fugiat dolores repellendus labore autem eius libero suscipit eveniet, nam tenetur. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur tenetur omnis placeat eveniet animi optio vitae, quae mollitia fuga quos excepturi saepe aliquam, dolorem vel? Dolore blanditiis magni aliquid hic. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate, ex. Velit, eum sed. Asperiores velit quis accusantium temporibus, est molestiae ipsam earum. Hic, eaque quaerat ab veritatis ipsa est architecto.')
-          // ]
         }
       ]
     });
@@ -189,10 +137,10 @@ function DocumentGenerator(props) {
   return (
     <div>
       <h2>{heading}</h2>
+      <button onClick={() => console.log('documentArray:', documentArray)}>console.log documentArray</button>
+      <button onClick={() => createChildrenArray(...documentArray)}>create childrenArray</button>
+      <button onClick={() => console.log('childrenArray:', childrenArray)}>console.log childrenArray</button>
       <button onClick={() => generate()}>generate document</button>
-      <button onClick={() => proceduralTest()}>procedural test</button>
-      <button onClick={() => createHeadersAndParagraphs(...documentArray)}>create headers and paragraphs test</button>
-      <p>{JSON.stringify(childrenArray)}</p>
     </div>
   );
 }
