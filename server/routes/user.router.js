88859d93b57c7959e72d0826f91bf6d535fcd5b4
Joshua McCollum <xmtuner629@yahoo.com>
2022-02-12 11:50:41 -0600

    updated answer data and added column in answer table

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -8,8 +8,10 @@ const userStrategy = require('../strategies/user.strategy');
 
 const router = express.Router();
 
+
 // Handles Ajax request for user information if user is authenticated
 router.get('/', rejectUnauthenticated, (req, res) => {
+ 
   // Send back user object from the session (previously queried from the database)
   res.send(req.user);
 });
