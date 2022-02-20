6c6bee7b9b65497ecefe2c2fe7701680c326c87f
Giancarlo Sierra <85034079+gsierra22@users.noreply.github.com>
2022-02-20 12:40:51 -0600

    Revert "Revert "Revert "styling and buttons"""

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -18,15 +18,7 @@ const [saveToggle, setSaveButton] = useState(false);
   return (
     <div className="container">
       <div>
-        <h1>Help Guide</h1>
-        <h3>Important Terminology</h3>
-        <p>OBT: Online Booking Tool (a tool which consolidates)</p>
-        <p>OTA: Online Travel Agency (a company/service such as Expedia, booking.com, Skyscanner, Google Flights)</p>
-        <h3>Questions</h3>
-        <h5>What other services does FerskTech offer?</h5>
-        <p>FerskTech offers state-of-the-art travel technology for corporate travel programs. This includes all elements of category management, from strategy, RFP, auditing, and ongoing optimization</p>
-        <h5>Does FerskTech offer further support for policy building?</h5>
-        <p>Yes! We would love to speak to you more. Please contact Vicki (vbschuur@fersktech.com) for more details.</p>
+        <p>This about page is for anyone to read!</p>
       </div>
     </div>
   );
