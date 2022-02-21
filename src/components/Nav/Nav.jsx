3da1be41023c0276fb2f5d4d6e1abbb5d957f0b5
Giancarlo Sierra <giancarlosierra2011@hotmail.com>
2022-02-21 10:57:13 -0600

    FAQ work

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -230,7 +230,7 @@ function Nav() {
                       </MenuItem>
                     </Link>
                       <MenuItem onClick={handleSave}>
-                        <Typography textAlign="center">Save and Exit</Typography>
+                        <Typography textAlign="center">Save</Typography>
                       </MenuItem>
                     <Link className="navLink"  to="/home">
                 <MenuItem onClick={logOut}>
